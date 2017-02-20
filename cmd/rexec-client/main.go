c6d541dd94ced72fedd75acf7df065ff36efb16d
Francois Hill <fhill@antennamagus.com>
2017-02-20 12:46:40 +0200

    feat: add client flags for server and command to exec/start

--- cmd/rexec-client/main.go
+++ cmd/rexec-client/main.go
@@ -1,44 +1,75 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
+	"os"
+
+	docopt "github.com/docopt/docopt-go"
 
 	"github.com/golang-devops/rexec/comms"
 )
 
 var (
-	serverAddress = flag.String("server-address", "127.0.0.1:50505", "The address of the server")
+	Version = "0.0.1"
 )
 
 func main() {
-	flag.Parse()
-	if *serverAddress == "" {
-		log.Fatal("server-address flag is missing")
-	}
+	usage := `
+		Rexec Client.
 
-	client, err := comms.NewConnectedClient(*serverAddress)
+		Usage:
+		rexec-client exec --server=<server_address> <exe_path> <args>...
+		rexec-client start --server=<server_address> <exe_path> <args>...
+		rexec-client -h | --help
+		rexec-client -v | --version
+
+		Options:
+		--server=<server_address>	The address to the server
+		-h --help     				Show this screen.
+		-v --version  				Show version.
+  	`
+
+	arguments, err := docopt.Parse(usage, nil, true, "Rexec Client "+Version, false)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	args := &comms.ExecutorExecuteArgs{Exe: "ping", Args: []string{"google.com"}}
+	serverAddress := arguments["--server"].(string)
 
-	/*executeReply := &comms.ExecutorExecuteReply{}
-	if err := client.Execute(args, executeReply); err != nil {
+	client, err := comms.NewConnectedClient(serverAddress)
+	if err != nil {
 		log.Fatal(err)
-	} else if executeReply.Error != nil {
-		log.Fatal(executeReply.Error)
-	}*/
-
-	startReply := &comms.ExecutorStartReply{}
-	if err := client.Start(args, startReply); err != nil {
-		log.Fatal(err)
-	} else if startReply.Error != nil {
-		log.Fatal(startReply.Error)
 	}
 
-	// fmt.Println(fmt.Sprintf("OUT: %s", string(executeReply.Out)))
-	fmt.Println(fmt.Sprintf("PID: %d", startReply.Pid))
+	if arguments["exec"].(bool) == true {
+		execExePath := arguments["<exe_path>"].(string)
+		execArgs := arguments["<args>"].([]string)
+		args := &comms.ExecutorExecuteArgs{Exe: execExePath, Args: execArgs}
+
+		executeReply := &comms.ExecutorExecuteReply{}
+		if err := client.Execute(args, executeReply); err != nil {
+			log.Fatal(err)
+		} else if executeReply.Error != nil {
+			log.Fatal(executeReply.Error)
+		}
+
+		fmt.Println(fmt.Sprintf("OUT: %s", string(executeReply.Out)))
+
+		os.Exit(0)
+	} else if arguments["start"].(bool) == true {
+		execExePath := arguments["<exe_path>"].(string)
+		execArgs := arguments["<args>"].([]string)
+		args := &comms.ExecutorExecuteArgs{Exe: execExePath, Args: execArgs}
+
+		startReply := &comms.ExecutorStartReply{}
+		if err := client.Start(args, startReply); err != nil {
+			log.Fatal(err)
+		} else if startReply.Error != nil {
+			log.Fatal(startReply.Error)
+		}
+		fmt.Println(fmt.Sprintf("Started with PID: %d", startReply.Pid))
+
+		os.Exit(0)
+	}
 }

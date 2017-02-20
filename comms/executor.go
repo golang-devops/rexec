938189ef5bc1727f848e260c257ecf9c9c0eeff2
Francois Hill <fhill@antennamagus.com>
2017-02-20 12:47:06 +0200

    feat: log to stdout when the server does exec/start

--- comms/executor.go
+++ comms/executor.go
@@ -1,6 +1,7 @@
 package comms
 
 import (
+	"fmt"
 	"net/rpc"
 	"os/exec"
 )
@@ -23,15 +24,25 @@ type ExecutorStartReply struct {
 }
 
 func (e *Executor) Execute(executeArgs *ExecutorExecuteArgs, reply *ExecutorExecuteReply) error {
+	fmt.Println(fmt.Sprintf("Execute. %q", append([]string{executeArgs.Exe}, executeArgs.Args...)))
+
 	cmd := exec.Command(executeArgs.Exe, executeArgs.Args...)
 	reply.Out, reply.Error = cmd.CombinedOutput()
 	return nil
 }
 
 func (e *Executor) Start(executeArgs *ExecutorExecuteArgs, reply *ExecutorStartReply) error {
+	pidStr := "NOT_SET"
+	defer func() {
+		fmt.Println(fmt.Sprintf("Start (pid = %s). %q", pidStr, append([]string{executeArgs.Exe}, executeArgs.Args...)))
+	}()
+
 	cmd := exec.Command(executeArgs.Exe, executeArgs.Args...)
 	reply.Error = cmd.Start()
 	reply.Pid = cmd.Process.Pid
+
+	pidStr = fmt.Sprintf("%d", reply.Pid)
+
 	return nil
 }
 
